99b16611a923a526e331bb2fb3acc6f1de0b1891
somto <onwuajuesesomtochukwu@gmail.com>
2023-11-06 09:27:13 +0100

    partial fulfillment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { ethers } from "ethers";
-import BUNN_ICO_ABI from "./assets/BUNN_ICO_ABI.json";
-
+// import { BaseContract, ethers, providers } from "ethers";
+import { BUNN_ICO_ABI } from "./components/BUNN_ICO_ABI";
 import { SetStateAction, useState } from "react";
 import { Main } from "./components/Main/Main";
 import { ConnectWallet } from "./components/Connectwallet/Connectwallet";
@@ -10,13 +9,13 @@ import { Sidebar } from "./components/Sidebar/Sidebar";
 
 import "./App.css";
 // import { BrowserProvider } from "ethers";
-import { Contract } from "ethers";
+import { Contract, ethers } from "ethers";
 // import { ContractRunner } from "ethers";
 
 function App() {
   const [account, setAccount] = useState(null),
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    [, setProvider] = useState<unknown>(),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [provider, setProvider] = useState<any>(),
     [buttonText, setButtonText] = useState("Connect Wallet"),
     [contractAddress, setContractAddress] = useState<string>(""),
     [contract, setContract] = useState<Contract>(),
@@ -37,21 +36,17 @@ function App() {
         : alert("install An Ethereum Wallet");
 
       setContractAddress("0x846C9D65404B5325163f2850DAcF7C3Dff9ef0B2");
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      const _provider = new ethers.providers.Web3Provider(window.ethereum);
-
-      setSigner(_provider?.getSigner());
-
-      const _contract = new ethers.Contract(
-        contractAddress,
-        BUNN_ICO_ABI,
-        signer
+      setProvider(
+        new ethers.providers.JsonRpcProvider(
+          "https://eth-sepolia.g.alchemy.com/v2/uJ6VyGFM0IqVjGXqi0pjDyhbwpp7h58I"
+        )
       );
-      console.log("contract", _contract);
-      setContract(_contract);
 
-      setProvider(_provider);
+      setSigner(provider.getSigner());
+
+      setContract(new Contract(contractAddress, BUNN_ICO_ABI, signer));
+
+      console.log("contract", contract);
 
       // updateEthers();
     },
@@ -62,8 +57,7 @@ function App() {
     getBal = async () => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      const b = await contract.totalSupply();
-      setBalance(b);
+      setBalance(await contract.totalSupply());
     },
     // updateEthers = () => { },
     logout = async () => {
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,13 +3,13 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <code className="footer">
-      <a href="https://blockchainunn.org/" target="blanc">
+      <a href="https://blockchainunn.org/" target="BUNN">
         BlockchainUNN
       </a>{" "}
       WEB3 Bootcamp - Assignment 4
       <br />
       ...as submitted by
-      <a href="https://linktr.ee/somtochukwu1" target="blanc">
+      <a href="https://linktr.ee/somtochukwu1" target="somto">
         {" "}
         Somtochukwu .K. Onwuajuese
       </a>
